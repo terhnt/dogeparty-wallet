ca466d1a8d7bcd3e9b2e87daaebb9e0008379bba
JahPowerBit <lightzarlboro@gmail.com>
2014-07-22 11:37:19 +0000

    simplebuy step 1

--- src/js/pages.init.js
+++ src/js/pages.init.js
@@ -405,3 +405,12 @@ function initRPS() {
   RPS.init();
 }
 INIT_FUNC['pages/rps.html'] = initRPS;
+
+function initSimpleBuy() {
+  pageSetUp();
+  window.SIMPLE_BUY = new SimpleBuyViewModel();
+  ko.applyBindings(SIMPLE_BUY, document.getElementById("simplebuy"));
+
+  SIMPLE_BUY.init();
+}
+INIT_FUNC['pages/simplebuy.html'] = initSimpleBuy;
