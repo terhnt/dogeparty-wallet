639bceaa3e550467e1dd762fcf090552e11d7318
Andrew <95168952+terhnt@users.noreply.github.com>
2022-02-07 20:15:44 +1100

    Fix burn estimation amount

--- src/js/util.bitcoin.js
+++ src/js/util.bitcoin.js
@@ -97,11 +97,11 @@ function makeQRCode(addr) {
 
 function testnetBurnDetermineEarned(blockHeight, burned) {
   //burned is the quantity of BTC to burn (as a float -- normalized value)
-  //XCP quantity returned is as a float -- normalized value
+  //XUP quantity returned is as a float -- normalized value
   burned = denormalizeQuantity(burned);
   var total_time = TESTNET_BURN_END - TESTNET_BURN_START;
   var partial_time = TESTNET_BURN_END - blockHeight;
-  var multiplier = 1000 * (1 + .5 * (partial_time / total_time)); //will be approximate
+  var multiplier = 2000 * (500 * (partial_time / total_time)); //will be approximate
   var earned = Decimal.round(new Decimal(burned).mul(multiplier), 8, Decimal.MidpointRounding.ToEven).toFloat();
   return normalizeQuantity(earned);
 }
